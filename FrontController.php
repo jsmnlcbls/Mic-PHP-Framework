bb007e6a503941148cdca7f0064fc6d48b13bcdb
Jose Manuel A. Cabiles III <enilehcim@gmail.com>
2011-07-03 23:49:07 +0800

    Adding copyright notice on two files. (Settings.php and FrontController.php

--- FrontController.php
+++ FrontController.php
@@ -1,5 +1,24 @@
 <?php
 /**
+ * Copyright 2010, Jose Manuel A. Cabiles III
+ * 
+ * This file is part of Mic Framework.
+ * 
+ * Mic Framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Mic Framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Mic Framework.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
  * The Front Controller. One Controller To Rule Them All.
  * PHP processing starts here.
  * @author enilehcim
